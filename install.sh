84ee7cf54c7cada096a09de430ee71736cd30cd7
Miroslav Safr <Miroslav.Safr@gmail.com>
2012-02-23 11:31:26 +0200

    typo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-#fast script to create rpm package inside the git repo without beeing root  - https://github.com/safrm/rpmmake
+#fast script to create rpm package inside the git repo without being root  - https://github.com/safrm/rpmmake
 #author:  Miroslav Safr <miroslav.safr@gmail.com>
 BINDIR=/usr/local/bin
 
