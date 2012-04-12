4e15438cd76a0439e4e0042304d3c31f514f0e34
Miroslav Safr <Miroslav.Safr@gmail.com>
2012-04-12 11:55:48 +0300

    Chg: install.sh default to /usr/bin/

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 #fast script to create rpm package inside the git repo without being root  - https://github.com/safrm/rpmmake
 #author:  Miroslav Safr <miroslav.safr@gmail.com>
-BINDIR=/usr/local/bin
+BINDIR=/usr/bin
 
 sudo mkdir -p -m 0755 $BINDIR
 sudo install -m 0777 -v ./rpmmake  $BINDIR/
