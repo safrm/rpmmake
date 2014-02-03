0130afce83bef024ffe8e437f26e8e20e715b644
Miroslav Safr <Miroslav.Safr@gmail.com>
2014-02-03 11:04:03 +0200

    Fix: appver sourcing syntax
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>
    Signed-off-by: Miroslav Safr <Miroslav.Safr@tieto.com>

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ USERID=`id -u`
 }
 
 #automatic version 
-if [ command -v appver >/dev/null 2>&1 ]; then . appver; else APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
+if command -v appver &>/dev/null; then . appver; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
 
 for TEST in $(  grep -r -l -h "#\!/bin/sh" . )
 do
