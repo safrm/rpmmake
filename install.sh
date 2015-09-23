940f0b59d742cdca65016cf6d561e3d9f666f1a4
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-09-23 20:30:32 +0300

    appver-installer 1.3.0
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -1,48 +1,20 @@
 #/bin/sh
-#more than fast script to create rpm package inside the git repo without being root  - http://safrm.net/projects/rpmmake
+#more than fast script to create rpm package inside the git repo without being root 
+#web: http://safrm.net/projects/rpmmake
 #author:  Miroslav Safr <miroslav.safr@gmail.com>
-BINDIR=/usr/bin
-COMPLETION_DIR=/etc/bash_completion.d
-MANDIR=/usr/share/man
+. appver-installer
 
-#root check
-USERID=`id -u`
-[ $USERID -eq "0" ] || {
-    echo "I cannot continue, you should be root or run it with sudo!"
-    exit 0
-}
+appver_basic_scripts_test
 
-#automatic version
-if command -v appver 1>/dev/null 2>&1; then . appver; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
-#test
-for TEST in $(  grep -r -l -h "#\!/bin/sh" --exclude-dir=.git . )
-do
-		sh -n $TEST
-		if  [ $? != 0 ]; then
-			echo "syntax error in $TEST, exiting.." 
-			exit 1
-		fi
-done
+$MKDIR_755 $BINDIR
+$INSTALL_755 ./rpmmake  $BINDIR
+appver_update_version_and_date $BINDIR/rpmmake
+$INSTALL_755 ./rpmmake-changelog  $BINDIR
+appver_update_version_and_date $BINDIR/rpmmake-changelog
+$INSTALL_755 ./rpmmake-debchangelog  $BINDIR
+appver_update_version_and_date $BINDIR/rpmmake-debchangelog
+$INSTALL_755 ./rpmmake-expect  $BINDIR
 
-#update documentation
-jss-docs-update ./doc 
-
-mkdir -p -m 0755 $BINDIR
-install -m 0777 -v ./rpmmake  $BINDIR/
-sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/rpmmake && rm -f $BINDIR/rpmmake.bkp
-sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/rpmmake && rm -f $BINDIR/rpmmake.bkp
-install -m 0777 -v ./rpmmake-changelog  $BINDIR/
-sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/rpmmake-changelog && rm -f $BINDIR/rpmmake-changelog.bkp
-sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/rpmmake-changelog && rm -f $BINDIR/rpmmake-changelog.bkp
-install -m 0777 -v ./rpmmake-debchangelog  $BINDIR/
-sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/rpmmake-debchangelog && rm -f $BINDIR/rpmmake-debchangelog.bkp
-sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/rpmmake-debchangelog && rm -f $BINDIR/rpmmake-debchangelog.bkp
-install -m 0777 -v ./rpmmake-expect  $BINDIR/
-
-mkdir -p -m 0755 $COMPLETION_DIR
-install -m 0777 -v ./rpmmake_completion  $COMPLETION_DIR/
-
-MANPAGES=`find ./doc/manpages -type f`
-install -d -m 755 $MANDIR/man1
-install -m 644 $MANPAGES $MANDIR/man1
+$MKDIR_755 $COMPLETION_DIR
+$INSTALL_755 ./rpmmake_completion  $COMPLETION_DIR
 
